a04b1aa1d081df6d571d657be54c4ab545911e22
Damien <boenapfel.damien@yahoo.fr>
2024-12-05 16:29:27 +0100

    maj

--- requete.sql
+++ requete.sql
@@ -80,6 +80,34 @@ UPDATE recipe
 SET preparation_time = preparation_time - 5 -- NE PAS OUBLIER DE SOUSTRAIRE A LA VALEUR ACTUEL
 
 -- Affiche les recettes sans ingrédients coutant + de 2€
+SELECT recipe_name -- sélectionne toutes les recettes qui ne sont pas (NOT IN) concernées 
+FROM recipe
+WHERE id_recipe NOT IN (
+	SELECT id_recipe -- récupère toutes les recettes dont au moins un ingrédient coûte 2 euros 
+	FROM recipe_ingredients
+	INNER JOIN ingrediant ON recipe_ingredients.id_ingredient = ingrediant.id_ingrediant
+	WHERE recipe_ingredients.id_recipe = recipe.id_recipe -- extrait la ligne si id recipe = & price > 2
+	AND ingrediant.price > 2 );
 
+-- Affiche la recette plus rapide a preparé
+SELECT recipe_name, preparation_time
+FROM recipe
+WHERE preparation_time = (SELECT MIN(preparation_time) FROM recipe)
 
--- Ajoute un ingredient à une recette
+-- trouver les recettes qui nécessitent aucun ingredient
+SELECT recipe_name
+FROM recipe
+WHERE id_recipe NOT IN ( -- filtre les recettes qui n'ont pas d'ingredient
+	SELECT id_recipe
+	FROM recipe_ingredients);
+
+-- trouve les ingredients utilisés dans au moins 3 recettes
+SELECT ingrediant_name, COUNT(recipe_ingredients.id_recipe)
+FROM ingrediant
+INNER JOIN recipe_ingredients ON ingrediant.id_ingrediant = recipe_ingredients.id_ingredient
+GROUP BY ingrediant.id_ingrediant
+HAVING COUNT(recipe_ingredients.id_recipe) >= 3;
+
+-- ajouter un ingredient a une recette
+INSERT INTO recipe_ingredients (quantity, unity, id_ingredient, id_recipe)
+VALUES (1, "L", 5, 1); -- Insert 1 litre de (id=5)Sauce César dans (id=1)Salade césar
